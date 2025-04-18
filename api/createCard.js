1bcacb13995745a6f76e7545a9212f09af388052
ArthurYamanaka <arthur.yamanaka@outlook.com.br>
2025-04-18 01:10:23 -0400

    Update createCard.js

--- api/createCard.js
+++ api/createCard.js
@@ -5,20 +5,29 @@ export default async function handler(req, res) {
 
   const { message, due, labels } = req.body;
 
+  const labelMap = {
+    "prioridade-baixa": "662061b410c59e51939d9fd1",
+    "prioridade-alta": "662061bc10c59e51939d9fd7",
+    "urgente": "SEU_ID_URGENTE",
+    "importante": "SEU_ID_IMPORTANTE"
+  };
+
+  const labelIds = (labels || []).map(label => labelMap[label] || label);
+
   try {
     const response = await fetch(`https://api.trello.com/1/cards`, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify({
         name: message,
         due,
-        idLabels: labels,
+        idLabels: labelIds,
         idList: process.env.LIST_ID,
         key: process.env.TRELLO_KEY,
-        token: process.env.TRELLO_TOKEN,
-      }),
+        token: process.env.TRELLO_TOKEN
+      })
     });
 
     const data = await response.json();
