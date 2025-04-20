3c5fc3a8ee2dd84b7dcee30dc280dbbbccfc9032
ArthurYamanaka <arthur.yamanaka@outlook.com.br>
2025-04-20 00:50:29 -0400

    Update getCards.js

--- api/getCards.js
+++ api/getCards.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
@@ -34,4 +36,4 @@ export default async function handler(req, res) {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
