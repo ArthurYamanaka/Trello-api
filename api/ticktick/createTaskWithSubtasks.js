dd666274e727cbb18438788bc36b9e1e46773bf9
ArthurYamanaka <arthur.yamanaka@outlook.com.br>
2025-04-19 23:33:10 -0400

    Update createTaskWithSubtasks.js

--- api/ticktick/createTaskWithSubtasks.js
+++ api/ticktick/createTaskWithSubtasks.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const sessionToken = '154BB8FE914467833344C66821ABCBD0916CEDEA65DDC12DD668166DD29E1D1354DDA0DF15636D00119801F4D87D7BFAC9962465C8A1A5CF1CA5A43983C43F096EF38D9D95F6FCF3F4CA2CEC9F2DE05AF336BE3358DCA9E1737B9EDFA8624DEE82BEE463B1431075BC4DD36207DCA5A8F336BE3358DCA9E1D64E2D2CD19AF6022BB8BB67B572BDA342581C63FE29D8A4C095F6809D58690368144EDDDF7DE7A1E758EE688C2CE33E39311D05FCBE9955'; // substitua se ainda não estiver no ambiente seguro
+const sessionToken = 'SEU_TOKEN_AQUI'; // Substitua por variável de ambiente depois
 
 const headers = {
   "Content-Type": "application/json",
@@ -25,9 +25,7 @@ const criarTarefaPrincipal = async () => {
   }, { headers });
 
   const tarefaCriada = response.data.id2etag;
-  const parentId = Object.keys(tarefaCriada)[0];
-
-  return parentId;
+  return Object.keys(tarefaCriada)[0];
 };
 
 const criarSubtasks = async (parentId, subtasks) => {
@@ -59,6 +57,7 @@ const vincularSubtasks = async (parentId, projectId, subtasksIds) => {
   await axios.post('https://api.ticktick.com/api/v2/batch/taskParent', payload, { headers });
 };
 
+// ESTE BLOCO FINAL É O MAIS IMPORTANTE PARA FUNCIONAR NA VERCEL
 module.exports = async (req, res) => {
   try {
     const parentId = await criarTarefaPrincipal();
